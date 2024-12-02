04a1fdaadaf1c40be6dd54916ef6480a8ba908dd
Ruan Meyer <rmeyer.dev@gmail.com>
2024-12-02 15:14:07 +0000

    links added to navbar icons

--- components/navbar.vue
+++ components/navbar.vue
@@ -8,9 +8,15 @@
       <a class="item" href="/">CONTACT</a>
     </div>
     <div class="flex row justify-end">
-      <LucideFacebook class="ico"/>
-      <LucideInstagram class="ico"/>
-      <LucideMapPinned class="ico"/>
+      <a href="https://www.facebook.com/people/Atlantic-Tattoo-Art/61559306673874/" target="_blank">
+        <LucideFacebook class="ico"/>
+      </a>
+      <a href="https://www.instagram.com/atlantic_ink/" target="_blank">
+        <LucideInstagram class="ico"/>
+      </a>
+      <a href="https://www.google.com/maps/place/Atlantic+Tattoo+Art/@28.0522495,-16.7187311,970m/data=!3m2!1e3!4b1!4m6!3m5!1s0xc6a99fe8c803eff:0x69a83892a8126daf!8m2!3d28.0522448!4d-16.7161562!16s%2Fg%2F11y47lcqss?entry=ttu&g_ep=EgoyMDI0MTEyNC4xIKXMDSoASAFQAw%3D%3D" target="_blank">
+        <LucideMapPinned class="ico"/>
+      </a>
     </div>
  </div>
 </template>
